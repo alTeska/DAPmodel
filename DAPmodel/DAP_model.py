2362ff9972c6a9834438ce5ec154ee32bd8d5dac
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-10-17 21:11:25 +0200

    all ion channels changed to dicts

--- DAPmodel/DAP_model.py
+++ DAPmodel/DAP_model.py
@@ -7,6 +7,7 @@ class DAP_Model():
     """
     nois_fact_obs = 0.
     seed = None
+
     if seed is not None:
         rng = np.random.RandomState(seed=seed)
     else:
@@ -15,83 +16,76 @@ class DAP_Model():
     cm = 0.63      #* 1e-6  # uF
     diam = 50.0    * 1e-4  # cm
     L = 100.0      * 1e-4  # cm
-    cell_area = diam * L * np.pi
     Ra = 100.0
+    cell_area = diam * L * np.pi
 
-    e_hcn = -29.46    #* 1e-3  # mV
-    e_nap = 60.0      #* 1e-3  # mV
-    e_nat = 60.0      #* 1e-3  # mV
-    e_leak = -86.53   #* 1e-3  # mV
+    e_hcn = -29.46    # mV
+    e_nap = 60.0      # mV
+    e_nat = 60.0      # mV
+    e_leak = -86.53   # mV
+    e_kdr = -110.0    # mV
     g_leak = 0.000430
-
-    # kdr
-    n_pow_kdr = 4
-
-
-    n_vh_kdr = -68.29         # * 1e-3  # mV
-    n_vs_kdr = 18.84          # * 1e-3  # mV
-    n_tau_min_kdr = 0.286     # * 1e-3  # ms
-    n_tau_max_kdr = 21.286    # * 1e-3  # ms
-    n_tau_delta_kdr = 0.746   # * 1e-3  # ms
-
-
-    e_kdr = -110.0    #* 1e-3  # mV
-    gbar_kdr = 0.00313   # (S/cm2)
+    gbar_kdr = 0.00313  # (S/cm2)
+    gbar_hcn = 5e-05    # (S/cm2)
+    gbar_nap = 0.01527  # (S/cm2)
+    gbar_nat = 0.142    # (S/cm2)
 
     kdr_n = {
-        'pow' : 4,
-        'vh' : -68.29,         # mV
-        'vs' : 18.84,         # mV
-        'tau_min' : 0.286,     # ms
-        'tau_max' : 21.286,    # ms
-        'tau_delta' : 0.746,   # ms
+        'pow': 4,
+        'vh': -68.29,         # mV
+        'vs': 18.84,          # mV
+        'tau_min': 0.286,     # ms
+        'tau_max': 21.286,    # ms
+        'tau_delta': 0.746,   # ms
     }
 
-    # hcn_slow
-    n_pow_hcn = 1
-
-    gbar_hcn = 5e-05   # (S/cm2)
-
-    n_vh_hcn = -77.9        # * 1e-3  # mV
-    n_vs_hcn = -20.54       # * 1e-3  # mV
-    n_tau_min_hcn = 2.206   # * 1e-3  # ms
-    n_tau_max_hcn = 137.799 # * 1e-3  # ms
-    n_tau_delta_hcn = 0.21  # * 1e-3  # ms
-
+    hcn_n = {
+        'pow': 1,
+        'vh': -77.9,         # mV
+        'vs': -20.54,        # mV
+        'tau_min': 2.206,    # ms
+        'tau_max': 137.799,  # ms
+        'tau_delta': 0.21,   # ms
+    }
 
     # Nat
-    gbar_nat = 0.142   # (S/cm2)
 
-    m_pow_nat = 3
-    h_pow_nat = 1
+    nat_m = {
+        'pow': 3,
+        'vs': 11.99,         # mV
+        'vh': -30.94,        # mV
+        'tau_min': 0,        # ms
+        'tau_max': 0.193,    # ms
+        'tau_delta': 0.187,  # ms
+    }
 
-    m_vs_nat = 11.99        #* 1e-3  # mV
-    h_vs_nat = -13.17       #* 1e-3  # mV
-    m_vh_nat = -30.94       #* 1e-3  # mV
-    h_vh_nat = -60.44       #* 1e-3  # mV
-    m_tau_min_nat = 0       #* 1e-3  # ms
-    h_tau_min_nat = 0.001   #* 1e-3  # ms
-    m_tau_max_nat = 0.193   #* 1e-3  # ms
-    h_tau_max_nat = 8.743   #* 1e-3  # ms
-    h_tau_delta_nat = 0.44  #* 1e-3  # ms
-    m_tau_delta_nat = 0.187 #* 1e-3  # ms
+    nat_h = {
+        'pow': 1,
+        'vs': -13.17,       # mV
+        'vh': -60.44,       # mV
+        'tau_min': 0.001,   # ms
+        'tau_max': 8.743,   # ms
+        'tau_delta': 0.44,  # ms
+    }
 
     # Nap
-    gbar_nap = 0.01527  # (S/cm2)
+    nap_m = {
+        'pow': 3,
+        'vs': 16.11,         # mV
+        'vh': -52.82,        # mV
+        'tau_min': 0.036,    # ms
+        'tau_max': 15.332,   # ms
+        'tau_delta': 0.505,  # ms
+    }
 
-    m_pow_nap = 3  # p
-    h_pow_nap = 1  # q
-
-    m_vs_nap = 16.11    #* 1e-3  # mV
-    h_vs_nap = -19.19   #* 1e-3  # mV
-    m_vh_nap = -52.82   #* 1e-3  # mV
-    h_vh_nap = -82.54   #* 1e-3  # mV
-    m_tau_min_nap = 0.036   #* 1e-3  # ms
-    h_tau_min_nap = 0.336   #* 1e-3  # ms
-    m_tau_max_nap = 15.332  #* 1e-3  # ms
-    h_tau_max_nap = 13.659  #* 1e-3  # ms
-    m_tau_delta_nap = 0.505 # * 1e-3  # ms
-    h_tau_delta_nap = 0.439 # * 1e-3  # ms
+    nap_h = {
+        'pow': 1,  
+        'vs': -19.19,        # mV
+        'vh': -82.54,        # mV
+        'tau_min': 0.336,    # ms
+        'tau_max': 13.659,   # ms
+        'tau_delta': 0.439,  # ms
+    }
 
     def temp_corr(self, temp):
         '''temperature correction'''
@@ -104,11 +98,7 @@ class DAP_Model():
     def dx_dt(self, x, x_inf, x_tau):
         return (x_inf - x) / x_tau
 
-    def x_tau(self, V, x_tau_min, x_tau_max, xinf, x_tau_delta, x_vs, x_vh):
-        return (x_tau_min + (x_tau_max - x_tau_min) * xinf * \
-                np.exp(x_tau_delta * (x_vh - V) / x_vs))
-
-    def x_tau_dict(self, V, xinf, ion_ch):
+    def x_tau(self, V, xinf, ion_ch):
         return (ion_ch['tau_min'] + (ion_ch['tau_max'] - ion_ch['tau_min']) * \
                 xinf * np.exp(ion_ch['tau_delta'] * \
                 (ion_ch['vh'] - V) / ion_ch['vs']))
@@ -120,8 +110,6 @@ class DAP_Model():
     def i_k(self, V, n, gbar, n_pow, e_ion):
         return (gbar * self.cell_area) * n**n_pow * (V - e_ion)
 
-    def i_k_dict(self, V, n, gbar, n_pow, e_ion):
-        return (gbar * self.cell_area) * n**n_pow * (V - e_ion)
 
 
     def simulate(self, T, dt, i_inj):
@@ -139,48 +127,30 @@ class DAP_Model():
         N_kdr = np.zeros_like(t)
 
         U[0] = -75 #* 1e-3   # mV
-        M_nap[0] = self.x_inf(U[0], self.m_vh_nap, self.m_vs_nap)
-        M_nat[0] = self.x_inf(U[0], self.m_vh_nat, self.m_vs_nat)
-        H_nap[0] = self.x_inf(U[0], self.h_vh_nap, self.h_vs_nap)
-        H_nat[0] = self.x_inf(U[0], self.h_vh_nat, self.h_vs_nat)
-        N_hcn[0] = self.x_inf(U[0], self.n_vh_hcn, self.n_vs_hcn)
-        N_kdr[0] = self.x_inf(U[0], self.n_vh_kdr, self.n_vs_kdr)
-
+        M_nap[0] = self.x_inf(U[0], self.nap_m['vh'], self.nap_m['vs'])
+        M_nat[0] = self.x_inf(U[0], self.nat_m['vh'], self.nat_m['vs'])
+        H_nap[0] = self.x_inf(U[0], self.nap_h['vh'], self.nap_h['vs'])
+        H_nat[0] = self.x_inf(U[0], self.nat_h['vh'], self.nat_h['vs'])
+        N_hcn[0] = self.x_inf(U[0], self.hcn_n['vh'], self.hcn_n['vs'])
+        N_kdr[0] = self.x_inf(U[0], self.kdr_n['vh'], self.kdr_n['vs'])
 
         for n in range(0, len(i_inj)-1):
-            # calculate steady states
-            M_nap_inf = self.x_inf(U[n], self.m_vh_nap, self.m_vs_nap)
-            M_nat_inf = self.x_inf(U[n], self.m_vh_nat, self.m_vs_nat)
-            H_nap_inf = self.x_inf(U[n], self.h_vh_nap, self.h_vs_nap)
-            H_nat_inf = self.x_inf(U[n], self.h_vh_nat, self.h_vs_nat)
-
-            N_hcn_inf = self.x_inf(U[n], self.n_vh_hcn, self.n_vs_hcn)
-            N_kdr_inf = self.x_inf(U[n], self.n_vh_kdr, self.n_vs_kdr)
+            # calculate x_inf
+            M_nap_inf = self.x_inf(U[n], self.nap_m['vh'], self.nap_m['vs'])
+            M_nat_inf = self.x_inf(U[n], self.nat_m['vh'], self.nat_m['vs'])
+            H_nap_inf = self.x_inf(U[n], self.nap_h['vh'], self.nap_h['vs'])
+            H_nat_inf = self.x_inf(U[n], self.nat_h['vh'], self.nat_h['vs'])
+            N_hcn_inf = self.x_inf(U[n], self.hcn_n['vh'], self.hcn_n['vs'])
+            N_kdr_inf = self.x_inf(U[n], self.kdr_n['vh'], self.kdr_n['vs'])
 
 
             # calcualte  time constants
-            tau_m_nap = self.x_tau(U[n], self.m_tau_min_nap, self.m_tau_max_nap,
-                                   M_nap_inf, self.m_tau_delta_nap, self.m_vs_nap,
-                                   self.m_vh_nap)
-            tau_m_nat = self.x_tau(U[n], self.m_tau_min_nat, self.m_tau_max_nat,
-                                   M_nat_inf, self.m_tau_delta_nat, self.m_vs_nat,
-                                   self.m_vh_nat)
-
-            tau_h_nap = self.x_tau(U[n], self.h_tau_min_nap, self.h_tau_max_nap,
-                                   H_nap_inf, self.h_tau_delta_nap,
-                                   self.h_vs_nap, self.h_vh_nap)
-            tau_h_nat = self.x_tau(U[n], self.h_tau_min_nat, self.h_tau_max_nat,
-                                   H_nat_inf, self.h_tau_delta_nat,
-                                   self.h_vs_nat, self.h_vh_nat)
-
-            tau_n_hcn = self.x_tau(U[n], self.n_tau_min_hcn, self.n_tau_max_hcn,
-                                   N_hcn_inf, self.n_tau_delta_hcn,
-                                   self.n_vs_hcn, self.n_vh_hcn)
-            # tau_n_kdr = self.x_tau(U[n], self.n_tau_min_kdr, self.n_tau_max_kdr,
-            #                        N_kdr_inf, self.n_tau_delta_kdr,
-            #                        self.n_vs_kdr, self.n_vh_kdr)
-
-            tau_n_kdr = self.x_tau_dict(U[n], N_kdr_inf, self.kdr_n)
+            tau_m_nap = self.x_tau(U[n], M_nap_inf, self.nap_m)
+            tau_h_nap = self.x_tau(U[n], H_nap_inf, self.nap_h)
+            tau_m_nat = self.x_tau(U[n], M_nat_inf, self.nat_m)
+            tau_h_nat = self.x_tau(U[n], H_nat_inf, self.nat_h)
+            tau_n_hcn = self.x_tau(U[n], N_hcn_inf, self.hcn_n)
+            tau_n_kdr = self.x_tau(U[n], N_kdr_inf, self.kdr_n)
 
 
             # calculate all steady states
@@ -194,16 +164,13 @@ class DAP_Model():
 
             # calculate ionic currents
             i_nap = self.i_na(U[n], M_nap[n+1], H_nap[n+1], self.gbar_nap,
-                              self.m_pow_nap, self.h_pow_nap, self.e_nap)
-
+                              self.nap_m['pow'], self.nap_h['pow'], self.e_nap)
             i_nat = self.i_na(U[n], M_nat[n+1], H_nat[n+1], self.gbar_nat,
-                              self.m_pow_nat, self.h_pow_nat, self.e_nat)
-
-            i_hcn = self.i_k(U[n], N_hcn[n+1], self.gbar_hcn, self.n_pow_hcn,
+                              self.nat_m['pow'], self.nat_h['pow'], self.e_nat)
+            i_hcn = self.i_k(U[n], N_hcn[n+1], self.gbar_hcn, self.hcn_n['pow'],
                              self.e_hcn)
-            i_kdr = self.i_k(U[n], N_kdr[n+1], self.gbar_kdr, self.n_pow_kdr,
+            i_kdr = self.i_k(U[n], N_kdr[n+1], self.gbar_kdr, self.kdr_n['pow'],
                              self.e_kdr)
-
 
             i_ion = (i_nap + i_nat + i_kdr + i_hcn) * 1e3
 
