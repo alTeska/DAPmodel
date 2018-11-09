df478f054cdb2f51ba9bf4d707bd90b721649b44
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-11-09 14:12:09 +0100

    updating dap model test before backward eurler implementation

--- test_dap_model.py
+++ test_dap_model.py
@@ -10,8 +10,6 @@ dt = 0.01
 params, labels = obs_params()
 I, t, t_on, t_off = syn_current(duration=150, dt=dt)
 
-
-params = np.array([13, 16, 15])
 # define model
 dap1 = DAP(-75, params)
 hh1 = HH(init=[-75], params=[[50, 5]])
@@ -32,19 +30,5 @@ ax[0].plot(t, UDap, label='DAP');
 ax[0].plot(t, Uhh, label='hh');
 ax[0].legend()
 ax[1].plot(t, I);
-
-# plot of activation functions
-# fig, ax = plt.subplots(ncols=1, figsize=(20, 10));
-# ax.grid()
-# ax.set_ylabel('x')
-# ax.set_xlabel('t (ms)')
-#
-# ax.plot(t, M_nap, label='M_nap');
-# ax.plot(t, M_nat, label='M_nat');
-# ax.plot(t, H_nap, label='H_nap');
-# ax.plot(t, H_nat, label='H_nat');
-# ax.plot(t, N_kdr, label='N_kdr');
-# ax.plot(t, N_hcn, label='N_hcn');
-# plt.legend();
 
 plt.show()
