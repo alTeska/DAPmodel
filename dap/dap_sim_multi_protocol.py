c0810ee544d900ae80e0049adcf7703be02c8698
Aleksandra Teska <aleksandra.teska@gmail.com>
2019-04-10 13:06:02 +0200

    file init

---
+++ dap/dap_sim_multi_protocol.py
@@ -0,0 +1,70 @@
+# model based on lfimodels library by Jan-Matthis Lückmann
+import numpy as np
+
+from delfi.simulator.BaseSimulator import BaseSimulator
+from dap import DAP, DAPExp, DAPBe, DAPFeExp, DAPcython
+
+def param_transform(prior_log, x):
+    if prior_log:
+        return np.log(x)
+    else:
+        return x
+
+def param_invtransform(prior_log, x):
+    if prior_log:
+        return np.exp(x)
+    else:
+        return x
+
+class DAPSimulator(BaseSimulator):
+    def __init__(self, I, dt, V0, dim_param=4, prior_log=False, seed=None):
+        """
+        DAP simulator
+
+        Parameters
+        ----------
+        I : array
+            Numpy array with the input I
+        dt : float
+            Timestep
+        V0 : float
+            Voltage at first time step
+        seed : int or None
+            If set, randomness across runs is disabled
+        """
+        super().__init__(dim_param=dim_param, seed=seed)
+        self.I = I
+        self.dt = dt
+        self.t = np.linspace(0, len(I), len(I))*self.dt
+
+        self.prior_log = prior_log
+        self.init = [V0]
+
+
+    def gen_single(self, params):
+        """Forward model for simulator for single parameter set
+
+        Parameters
+        ----------
+        params : list or np.array, 1d of length dim_param
+            Parameter vector
+        integration : string deciding on the type of integration used
+
+        Returns
+        -------
+        dict : dictionary with data
+            The dictionary must contain a key data that contains the results of
+            the forward run. Additional entries can be present.
+        """
+        params = param_invtransform(self.prior_log,np.asarray(params))
+        assert params.ndim == 1, 'params.ndim must be 1'
+
+        dap_seed = self.gen_newseed()
+
+        dap = DAPcython(self.init, params, seed=dap_seed)
+        states = dap.simulate(self.dt, self.t, self.I)
+
+        return {'data': states.reshape(-1),
+                'time': self.t,
+                'dt': self.dt,
+                'I': self.I.reshape(-1)}
---
+++ tests/test_dap_sim_multi_protocol.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from dap.dap_simulator import DAPSimulator
+from dap.utils import obs_params_gbar, syn_current
+
+
+params, labels = obs_params_gbar()
+I, t, t_on, t_off = syn_current(duration=150, dt=0.01)
+
+# define model
+dap1 = DAPSimulator(I, 0.01, -75)
+
+# run model
+stats = dap1.gen_single(params)
+
+# plot voltage trace
+fig, ax = plt.subplots(ncols=1, nrows=2, figsize=(20, 10));
+ax[0].grid()
+ax[0].set_ylabel('V (mV)')
+ax[0].set_xlabel('t (ms)')
+ax[0].plot(stats['time'], stats['data'], label='DAP');
+ax[0].legend()
+ax[1].plot(t, I);
+
+
+plt.show()
